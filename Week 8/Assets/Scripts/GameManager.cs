be483362ad461d765ed6dfd97dc4ed4760171fdc
Zhiyue Huang <zhiyuehuang@yeah.net>
2021-03-29 10:51:46 +0800

    Uploaded a game

---
+++ Week 8/Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public ScenarioScriptableObject currentScenario;
+
+    public Text scenarioNameText;
+    public Text scenarioDescription;
+    public Text scenarioQuestion;
+
+    public GameObject buttonYes;
+    public GameObject buttonNo;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScenario();
+    }
+
+    public void MoveScenario(int dir)
+    {
+        // go to different scenarios based on players' choices
+        switch (dir)
+        {
+            case 0:
+                currentScenario = currentScenario.scenarioYes;
+                break;
+            case 1:
+                currentScenario = currentScenario.scenarioNo;
+                break;
+            default:
+                break;
+        }
+        
+        UpdateScenario();
+    }
+    
+    void UpdateScenario()
+    {
+        // the display of texts
+        scenarioNameText.text = currentScenario.scenarioName;
+        scenarioDescription.text = currentScenario.description;
+        scenarioQuestion.text = currentScenario.question;
+
+        // turn of the buttons when there is no choice
+        if (currentScenario.scenarioYes == null)
+        {
+            buttonYes.SetActive(false);
+        }
+        else
+        {
+            buttonYes.SetActive(true);
+        }
+        
+        if (currentScenario.scenarioNo == null)
+        {
+            buttonNo.SetActive(false);
+        }
+        else
+        {
+            buttonNo.SetActive(true);
+        }
+    }
+}
---
+++ Week 8/Assets/Scripts/Scriptable Objects/ScenarioScriptableObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//create a menu
+[CreateAssetMenu (fileName = "New Scenario",
+    menuName = "ScriptableObjects/Scenario", order = 0)]
+
+public class ScenarioScriptableObject : ScriptableObject
+{
+    public string scenarioName = "New Scenario";
+    public string description = "Default Description";
+    public string question = "Default Question";
+
+    public ScenarioScriptableObject scenarioYes;
+    public ScenarioScriptableObject scenarioNo;
+}
